d208bee47de5d75d242fe02a902528d089927e2c
AREEBA-FATIMA <areebafatima2457@gmail.com>
2024-05-22 08:21:25 +0500

    code has been little bit updated

--- index.ts
+++ index.ts
@@ -1,27 +1,49 @@
 #! /usr/bin/env node
+import { differenceInSeconds } from "date-fns";
 import inquirer from "inquirer";
 
 async function main() {
     let ask = await inquirer.prompt({
         type: "number",
         message: "Please set timer in seconds",
-        name: "seconds"
+        name: "seconds",
+        validate:(input)=>{
+            if(isNaN(input)){
+                return "Please enter a valid number"
+            }
+            else if(input>60){
+                return "Seconds must be in 60"
+            }
+            else{
+                return true
+            }
+        }
     });
 
     const timerDuration = ask.seconds;
 
-    let remainingSeconds = timerDuration;
+    function startTime(val:number) {
+        let intTime = new Date().setSeconds(new Date().getSeconds()+ timerDuration)
+        let intervalTime = new Date(intTime)
+        setInterval ((()=>{
+            const currentTime =new Date()
+            let timeDiff = differenceInSeconds(intervalTime, currentTime)
 
-    const intervalId = setInterval(() => {
-        remainingSeconds--;
+            if (timeDiff<=0) {
+                console.log("Timer has expired!");
+                process.exit()
+            }
 
-        if (remainingSeconds <= 0) {
-            clearInterval(intervalId);
-            console.log("Timer completed!");
-        } else {
-            console.log(`Time remaining: ${remainingSeconds} seconds`);
-        }
-    }, 1000);
+        let min = Math.floor((timeDiff)%(3600*24)/3600)
+        let sec = Math.floor((timeDiff)%60)
+
+        console.log(`${min.toString().padStart(2,"0")}:${sec.toString().padStart(2,"0")}`);
+        
+        }),1000)
+    }
+
+    startTime(timerDuration)
 }
 
 main();
+ 
